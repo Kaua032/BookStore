c3776ea93cfcc5bb1fd678ffe4afb3af060d33d3
Kauã Xavier <kauaxavier032@gmail.com>
2023-10-09 21:29:46 -0300

    feat: Updated the mongoDBURL to include the password and added a route to create a book.

--- backend/config.js
+++ backend/config.js
@@ -1,3 +1,3 @@
 export const PORT = 5555
 
-export const mongoDBURL = "mongodb+srv://root:<password>@books-store-mern.cyz87eu.mongodb.net/?retryWrites=true&w=majority"
+export const mongoDBURL = "mongodb+srv://root:88124927kaua@books-store-mern.cyz87eu.mongodb.net/?retryWrites=true&w=majority"
--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,33 @@
 import express from "express";
 import { PORT, mongoDBURL } from "./config.js";
-import mongoose from "mongoose";
+import mongoose from "mongoose"; 
+import { Book } from "./models/bookModel.js";
 
 const app = express();
 
 app.get("/", (req, res) => {
   return res.status(234).send("Welcome to MERN Stack Tutorial");
+});
+
+app.post("/books", async (req, res) => {
+  try {
+    if (!req.body.title || !req.body.author || !req.body.publishYear) {
+      return res.status(400).send({
+        message: "Send all required fields: title, author, publishYear",
+      });
+    }
+
+    const newBook = {
+      title: req.body.title,
+      author: req.body.author,
+      publishYear: req.body.publishYear,
+    };
+
+    const book = await Book.create(newBook)
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).send({ message: error.message });
+  }
 });
 
 mongoose
