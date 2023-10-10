edacca6d1c61edab46f08c40c8306066805c8d18
Kauã Xavier <kauaxavier032@gmail.com>
2023-10-09 22:11:03 -0300

    feat: Added PUT route for updating books

--- backend/index.js
+++ backend/index.js
@@ -61,6 +61,29 @@ app.get("/books/:id", async (req, res) => {
   }
 });
 
+app.put("/books/:id", async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    if (!req.body.title || !req.body.author || !req.body.publishYear) {
+      return res.status(400).send({
+        message: "Send all required fields: title, author, publishYear",
+      });
+    }
+
+    const result = await Book.findByIdAndUpdate(id, req.body);
+
+    if (!result) {
+      return res.status(404).json({ message: "Book not found" });
+    }
+
+    return res.status(200).send({ message: "Book updated successfully" });
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).send({ message: error.message });
+  }
+});
+
 mongoose
   .connect(mongoDBURL)
   .then(() => {
