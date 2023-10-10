333a53e62f0bbf390938a8f2684c7c5122b539cc
Kauã Xavier <kauaxavier032@gmail.com>
2023-10-09 23:07:53 -0300

    feat: The diff removes the cors package and its configuration.

--- backend/index.js
+++ backend/index.js
@@ -7,13 +7,15 @@ import cors from "cors";
 const app = express();
 
 app.use(express.json());
-app.use(
-  cors({
-    origin: "http://localhost:5555",
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    allowedHeaders: ['Content-Type'],
-  })
-);
+
+app.use(cors());
+// app.use(
+//   cors({
+//     origin: "http://localhost:5555",
+//     methods: ['GET', 'POST', 'PUT', 'DELETE'],
+//     allowedHeaders: ['Content-Type'],
+//   })
+// );
 
 app.get("/", (req, res) => {
   return res.status(234).send("Welcome to MERN Stack Tutorial");
