7b984ba42fda6e081195b9d41d3abc84ddb0d6db
Kauã Xavier <kauaxavier032@gmail.com>
2023-10-10 00:38:03 -0300

    feat: Frontend: Create, edit, list books

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -15,7 +15,7 @@ const Home = () => {
     axios
       .get("http://localhost:5555/books")
       .then((response) => {
-        console.log(response)
+        console.log(response);
         setBooks(response.data.data);
         setLoading(false);
       })
@@ -32,28 +32,28 @@ const Home = () => {
         <Link to="/books/create">
           <MdOutlineAddBox className="text-sky-800 text-4xl" />
         </Link>
-        {loading ? (
-          <Spinner />
-        ) : (
-          <table className="w-full border-separate border-spacing-2">
-            <thead>
-              <tr>
-                <th className="border border-slate-600 rounded-md">No</th>
-                <th className="border border-slate-600 rounded-md">Title</th>
-                <th className="border border-slate-600 rounded-md max-md:hidden">
-                  Author
-                </th>
-                <th className="border border-slate-600 rounded-md max-md:hidden">
-                  Publish Year
-                </th>
-                <th className="border border-slate-600 rounded-md">
-                  Operations
-                </th>
-              </tr>
-            </thead>
-            <tbody>
-              {books.map((book, index) => {
-                return (<tr key={book._id} className="h-8">
+      </div>
+      {loading ? (
+        <Spinner />
+      ) : (
+        <table className="w-full border-separate border-spacing-2">
+          <thead>
+            <tr>
+              <th className="border border-slate-600 rounded-md">No</th>
+              <th className="border border-slate-600 rounded-md">Title</th>
+              <th className="border border-slate-600 rounded-md max-md:hidden">
+                Author
+              </th>
+              <th className="border border-slate-600 rounded-md max-md:hidden">
+                Publish Year
+              </th>
+              <th className="border border-slate-600 rounded-md">Operations</th>
+            </tr>
+          </thead>
+          <tbody>
+            {books.map((book, index) => {
+              return (
+                <tr key={book._id} className="h-8">
                   <td className="border border-slate-700 rounded-md text-center">
                     {index + 1}
                   </td>
@@ -80,12 +80,11 @@ const Home = () => {
                     </div>
                   </td>
                 </tr>
-                )
-              })}
-            </tbody>
-          </table>
-        )}
-      </div>
+              );
+            })}
+          </tbody>
+        </table>
+      )}
     </div>
   );
 };
