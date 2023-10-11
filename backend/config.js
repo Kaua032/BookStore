c1ee6d9fe2180309c701c0531bcffadd477e0a3f
Kauã Xavier <kauaxavier032@gmail.com>
2023-10-10 21:04:43 -0300

    feat: Update dependencies and add .env file

--- backend/config.js
+++ backend/config.js
@@ -1,3 +1,6 @@
-export const PORT = 5555
+import 'dotenv/config'
 
-export const mongoDBURL = "mongodb+srv://root:88124927kaua@books-store-mern.cyz87eu.mongodb.net/?retryWrites=true&w=majority"
+export const PORT = 5555;
+
+
+export const mongoDBURL = process.env.MONGO_DBURL;
